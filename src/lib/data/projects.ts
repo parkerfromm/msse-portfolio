825c759d0e54b8092657e1547e0a554370e13f03
Parker Fromm <pfromm@berkeley.edu>
2024-05-03 23:05:47 -0700

    project 3 and 4

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -9,6 +9,38 @@ export const items: Array<Project> = [
 		description:
 			'In this project, we combined data analysis and machine learning techniques to model air quality across various US counties. Using Python libraries NumPy, Pandas, seaborn, matplotlib, and scikit-learn, our team constructed two linear regression models and successfully conducted a comprehensive analysis of emission data.',
 		shortDescription:
+			'Neural Network model for predicting tau protein pregression in mice subject with genetic expression data.',
+		links: [{ to: 'https://github.com/Raj-Lab-UCSF/NexIS_SBI/tree/main', label: 'GitHub' }],
+		logo: Assets.Svelte,
+		name: 'Optimizing the NexIS - SBI mouse tauopathy model',
+		period: {
+			from: new Date()
+		},
+		skills: getSkills('python', 'jupyter'),
+		type: 'Capstone Project',
+	},
+	{
+		slug: 'slick-portfolio-svelte',
+		color: '#ff3e00',
+		description:
+			'In this project, we combined data analysis and machine learning techniques to model air quality across various US counties. Using Python libraries NumPy, Pandas, seaborn, matplotlib, and scikit-learn, our team constructed two linear regression models and successfully conducted a comprehensive analysis of emission data.',
+		shortDescription:
+			'API for creating cellar automata models, with example usage.',
+		links: [{ to: 'https://github.com/fractalclockwork/cellular_automata', label: 'GitHub' }],
+		logo: Assets.Svelte,
+		name: 'Cellular Automata',
+		period: {
+			from: new Date()
+		},
+		skills: getSkills('cpp', 'python'),
+		type: 'API',
+	},
+	{
+		slug: 'slick-portfolio-svelte',
+		color: '#ff3e00',
+		description:
+			'In this project, we combined data analysis and machine learning techniques to model air quality across various US counties. Using Python libraries NumPy, Pandas, seaborn, matplotlib, and scikit-learn, our team constructed two linear regression models and successfully conducted a comprehensive analysis of emission data.',
+		shortDescription:
 			'An accelerated neural network engine for predicting molecular energies.',
 		links: [{ to: 'https://github.com/parkerfromm/ani-final/tree/main/ani_proj', label: 'GitHub' }],
 		logo: Assets.Svelte,
